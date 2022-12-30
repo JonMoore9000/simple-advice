2f7ff17560fb6b7ecba639a4e859c0ec086f595e
JonMoore9000 <jonmoore9000@gmail.com>
2022-12-30 00:10:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Advice
+**Get some random but simple advice**
+
+##### A simple to get simple advice.
+
+You can test it here [Simple Advice](https://jonmoore9000.github.io/duelyst-randomizer/).
+
+![This is an image](home.png)
---
+++ app.js
@@ -0,0 +1,16 @@
+
+$('#clicker').on('click', function() {
+	num = Math.floor(Math.random()*220);
+  
+  var url = 'https://api.adviceslip.com/advice/' + num;
+
+function httpGet(url) {
+        let xmlHttpReq = new XMLHttpRequest();
+        xmlHttpReq.open("GET", url, false);
+        xmlHttpReq.send(null);
+        return xmlHttpReq.responseText;
+      }
+  
+	var advice = (JSON.parse(httpGet(url)));
+	$('#text').html(advice.slip.advice);
+})
